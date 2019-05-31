6557a46ab8819293f13e6bd5e7d7805a26a58589
Ben Findley <ben.g.findley@gmail.com>
2019-05-31 06:12:54 -0700

    buttons can get pressed

--- increment.js
+++ increment.js
@@ -1,13 +1,23 @@
 var button = document.getElementById("clicker");
+var buttonImg = document.getElementById("buttonImg");
 var unbutton = document.getElementById("unclicker");
+var unbuttonImg = document.getElementById("unbuttonImg");
 var clickCount = 0;
 var txtOut = document.getElementById("txt out");
 txtOut.innerHTML = clickCount.toString();
-button.onclick = function () {
+button.onmousedown = function () {
     clickCount++;
+    buttonImg.src = "./imgs/button_click.png";
     txtOut.innerHTML = clickCount.toString();
 };
-unbutton.onclick = function () {
+button.onmouseup = function () {
+    buttonImg.src = "./imgs/button.png";
+};
+unbutton.onmousedown = function () {
     clickCount--;
+    unbuttonImg.src = "./imgs/unbutton_click.png";
     txtOut.innerHTML = clickCount.toString();
+};
+unbutton.onmouseup = function () {
+    unbuttonImg.src = "./imgs/unbutton.png";
 };
--- increment.ts
+++ increment.ts
@@ -1,16 +1,26 @@
 var button = document.getElementById("clicker");
+var buttonImg = <HTMLImageElement>document.getElementById("buttonImg");
 var unbutton = document.getElementById("unclicker");
+var unbuttonImg = <HTMLImageElement>document.getElementById("unbuttonImg");
 
 var clickCount = 0;
 const txtOut: HTMLElement = document.getElementById("txt out");
 
 txtOut.innerHTML = clickCount.toString();
-button.onclick = () => {
+button.onmousedown = () => {
     clickCount++;
+    buttonImg.src = "./imgs/button_click.png";
     txtOut.innerHTML = clickCount.toString();
 }
+button.onmouseup = () => {
+    buttonImg.src = "./imgs/button.png";
+}
 
-unbutton.onclick = () => {
+unbutton.onmousedown = () => {
     clickCount--;
+    unbuttonImg.src = "./imgs/unbutton_click.png";
     txtOut.innerHTML = clickCount.toString();
+}
+unbutton.onmouseup = () => {
+    unbuttonImg.src = "./imgs/unbutton.png";
 }
