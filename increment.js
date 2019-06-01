8a5a88addefba99dfdc04ef89352a14b4c303395
Ben Findley <ben.g.findley@gmail.com>
2019-06-01 08:44:56 -0700

    purchasing improvements

--- increment.js
+++ increment.js
@@ -4,6 +4,42 @@ var unbutton = document.getElementById("unclicker");
 var unbuttonImg = document.getElementById("unbuttonImg");
 var clickCount = 0;
 var txtOut = document.getElementById("txt out");
+var Purchasable = /** @class */ (function () {
+    function Purchasable() {
+    }
+    return Purchasable;
+}());
+;
+var purchases = [{
+        button: document.getElementById("buyButtoneer"),
+        cost: 10,
+        value: 1,
+        count: 0
+    }, {
+        button: document.getElementById("buyUnbuttoneer"),
+        cost: -10,
+        value: -1,
+        count: 0
+    }
+];
+var _loop_1 = function (i) {
+    purchases[i].button.onmousedown = function () {
+        clickCount -= purchases[i].cost;
+        txtOut.innerHTML = clickCount.toString();
+        purchases[i].count++;
+    };
+};
+for (var i = 0; i < purchases.length; i++) {
+    _loop_1(i);
+}
+var dogametick = function () {
+    setTimeout(dogametick, 1000);
+    purchases.forEach(function (e) {
+        clickCount +=
+            e.count * e.value;
+    });
+    txtOut.innerHTML = clickCount.toString();
+};
 txtOut.innerHTML = clickCount.toString();
 button.onmousedown = function () {
     clickCount++;
@@ -21,3 +57,4 @@ unbutton.onmousedown = function () {
 unbutton.onmouseup = function () {
     unbuttonImg.src = "./imgs/unbutton.png";
 };
+dogametick();
--- increment.ts
+++ increment.ts
@@ -6,6 +6,43 @@ var unbuttonImg = <HTMLImageElement>document.getElementById("unbuttonImg");
 var clickCount = 0;
 const txtOut: HTMLElement = document.getElementById("txt out");
 
+class Purchasable {
+    button: HTMLButtonElement;
+    cost: number;
+    value: number;
+    count: number;
+};
+
+var purchases:Purchasable[] = [{
+        button: <HTMLButtonElement>document.getElementById("buyButtoneer"),
+        cost: 10,
+        value: 1,
+        count: 0,
+    },{
+        button: <HTMLButtonElement>document.getElementById("buyUnbuttoneer"),
+        cost: -10,
+        value: -1,
+        count: 0,
+    }
+]
+
+for(let i = 0; i < purchases.length; i++) {
+    purchases[i].button.onmousedown = () => {
+        clickCount -= purchases[i].cost;
+        txtOut.innerHTML = clickCount.toString();
+        purchases[i].count++;
+    }
+}
+
+const dogametick = () => {
+    setTimeout(dogametick, 1000);
+    purchases.forEach(e => {
+        clickCount += 
+            e.count * e.value;
+    });
+    txtOut.innerHTML = clickCount.toString();
+}
+
 txtOut.innerHTML = clickCount.toString();
 button.onmousedown = () => {
     clickCount++;
@@ -24,3 +61,5 @@ unbutton.onmousedown = () => {
 unbutton.onmouseup = () => {
     unbuttonImg.src = "./imgs/unbutton.png";
 }
+
+dogametick();
